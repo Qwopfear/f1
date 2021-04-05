24f7478d037e0f8ef097ec6f0a75b1e6d357adbc
Qwopfear <76784844+Qwopfear@users.noreply.github.com>
2021-04-05 13:14:17 +0200

    Add files via upload

---
+++ Autodina/script.js
@@ -0,0 +1,29 @@
+const title = document.querySelector('#title');
+const CLICKED_CLASS = 'clicked';
+
+
+function clickHandler(){
+  title.classList.toggle(CLICKED_CLASS);
+
+}
+function init(){
+  title.addEventListener('mouseenter', clickHandler);
+}
+
+
+init()
+
+const tytyl = document.querySelector('#tytyl');
+const CLICKED_CLAS = 'clicke';
+
+
+function clickHandler(){
+  title.classList.toggle(CLICKED_CLAS);
+
+}
+function init(){
+  title.addEventListener('mouseenter', clickHandler);
+}
+
+
+init()
